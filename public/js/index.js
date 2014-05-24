43d4d2d937694a19224f488890d5154cba31e061
chanhou <iepiechan@gmail.com>
2014-05-25 02:39:11 +0800

    mvc

---
+++ public/js/index.js
@@ -0,0 +1,36 @@
+var txt=$('#txt_name');
+var search=$('#search');
+
+search.on('click', function(){
+	console.log(txt.val());
+	$.ajax({
+		type:'GET',
+		url:'/search/'+txt.val(),
+		success:function(result){
+			console.log("search success");
+		}
+	});
+});
+
+$(document).ready(function() {
+
+	$(".various").fancybox({
+		// padding		:0,
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none',
+		helpers : {
+        overlay : {
+            locked : false// try changing to true and scrolling around the page
+        }
+    }
+	});
+});
+
+
