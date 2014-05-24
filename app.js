b14217d9496da871e078c06a2d16addc93931d02
oldwang <oldwang@junhaodeMacBook-Air.local>
2014-05-25 04:05:27 +0800

    basic REST

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ var flash = require('connect-flash');
 var mongoose = require('mongoose');
 var passport = require('passport');
 
+// testing 
+var fs = require('fs');
+var data = fs.readFileSync('models/db/gossip1.json');
+var tdata = JSON.parse(data);
+// console.log(typeof(tdata));
+// console.log(tdata[0].id);
+console.log(tdata.length);
+console.log(tdata[1].POST.length);
+console.log(tdata[1].REPO.length);
+// console.log(data.toString());
+
+
 // require('./models/db.js'); // TODO [DB] : Connect to database
 // require('./controllers/passport.js'); // TODO [FB] : Passport configuration
 
@@ -48,8 +60,26 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
-app.get('/s', function(req, res){
-  res.render('main');
+app.get('/s/:id', function(req, res){
+  if(0 <= req.params.id) {
+      res.statusCode = 404;
+    return res.send('Error 404: No quote found');
+  };
+  var id_itr;
+  for( id_itr=0; id_itr<tdata.length; id_itr++)
+    if( req.params.id === tdata[id_itr].id) break;
+  if( id_itr === tdata.length) {
+    res.statusCode = 404;
+    return res.send('Error 404: No quote found');
+  }
+  var list = tdata[id_itr].POST.concat(tdata[id_itr].REPO);
+  // var times_itr;
+  // for( times_itr=0; times_itr<list.length; times_itr++)
+  //   list[times_itr].time = toSecond(list[times_itr].time);
+  
+  //sortTime(list[0].time, list[1].time);
+
+  res.json(list);
 });
 
 
@@ -57,3 +87,13 @@ app.get('/s', function(req, res){
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
+// function sortTime( time1, time2){
+//   var res1=time1.split(" ");
+//   var res2=time2.split(" ");
+//   console.log(res1);
+//   console.log(res2);
+
+// }
