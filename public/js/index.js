891c0fb135835093684e45e177b7e7082d22edfb
chanhou <iepiechan@gmail.com>
2014-05-25 15:59:14 +0800

    1.0.z

--- public/js/index.js
+++ public/js/index.js
@@ -13,24 +13,38 @@ var search=$('#search');
 	});
 });
 
+txt.on('keyup',function(e){
+	if (e.which===13){
+		console.log(txt.val());
+		uurl='/search/'+txt.val();
+		$.ajax({
+			type:'GET',
+			success:function(result){
+				console.log("search success");
+				window.location=uurl;
+			}
+	});
+	}
+});
+
 $(document).ready(function() {
 
 	$(".various").fancybox({
 		// padding		:0,
-		maxWidth	: 800,
+		maxWidth	: 1000,
 		maxHeight	: 600,
 		fitToView	: false,
-		width		: '70%',
-		height		: '70%',
+		width		: '80%',
+		height		: '80%',
 		autoSize	: false,
 		closeClick	: false,
 		openEffect	: 'none',
 		closeEffect	: 'none',
 		helpers : {
-        overlay : {
-            locked : false// try changing to true and scrolling around the page
-        }
-    }
+	        overlay : {
+	            locked : false// try changing to true and scrolling around the page
+	        }
+    	}
 	});
 });
 
